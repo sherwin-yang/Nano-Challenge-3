ceb943748fa1da4a17ee3cffc07f8076b4289a6a
Sherwin Yang <sherwinyang@Sherwins-MacBook-Pro.local>
2020-06-11 00:05:18 +0700

    first commit

---
+++ NC3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NC3
+//
+//  Created by Sherwin Yang on 10/06/20.
+//  Copyright © 2020 Sherwin Yang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi Sherwin!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
